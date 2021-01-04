8a24064745a342444cfd07ac07a4c79a5defd241
daiki130 <daikibaseball130@gmail.com>
2021-01-04 13:25:27 +0900

    Jacket scrollAnimation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const SectionTopTitle = document.querySelector(".section-Top-title");
 const tl = new TimelineMax();
 const SectionTopLists = SectionTopTitle.querySelectorAll(".section-Top-title-list");
-const SectionProductMain = document.querySelectorAll(".section-product-main");
-const headLine = document.querySelector(".headline");
+const SectionProductMain = document.querySelector(".section-product-main");
+const SectionProductImgs = document.querySelector(".section-product-imgs");
+const ProductImgs = SectionProductImgs.querySelectorAll(".product-img");
 
 
 const cb = function(entries, observer) {
@@ -11,15 +12,15 @@ const cb = function(entries, observer) {
       SectionTopLists.forEach((SectionTopList, i) => {
         SectionTopList.classList.add(`boundAnimation${i}`);
       });
-      tl.fromTo(SectionProductMain, 1, {x: "-120%"}, {x: "0%", ease: Power2.easeInOut}, "+=2")
-        .to(SectionProductMain, 1, {opacity: "1", ease: Power2.easeInOut}, "-=2")
+      tl.fromTo(SectionProductMain, 1, {x: "-120%", opacity: "0"}, {x: "0%", opacity: "1", ease: Power2.easeInOut}, "+=2")
+      tl.fromTo(SectionProductImgs, 1, {x: "100%", opacity: "0"}, {x: "0%", opacity: "1", ease: Power2.easeInOut})
       observer.unobserve(entry.target); //監視の停止 entry.target = SectionTopTitleのこと
     }
   });
 }
 const options = {
   root: null,
-  rootMargin: "0px 0px -500px 0px", //画面の内側、下から300px監視対象の範囲
+  rootMargin: "0px 0px -300px 0px", //画面の内側、下から300px監視対象の範囲
   threshold: 1, //SectionTopTitleの下辺が500px内に入りきった時
 };
 const io = new IntersectionObserver(cb, options);
