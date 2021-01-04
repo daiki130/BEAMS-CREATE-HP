def8cfc3b4c71a67610d5aa37c2bff4fae907255
daiki130 <daikibaseball130@gmail.com>
2021-01-04 12:30:07 +0900

    main.js update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 const SectionTopTitle = document.querySelector(".section-Top-title");
 const tl = new TimelineMax();
-let SectionTopLists = [];
-SectionTopLists = SectionTopTitle.querySelectorAll(".section-Top-title-list");
+const SectionTopLists = SectionTopTitle.querySelectorAll(".section-Top-title-list");
+const SectionProductMain = document.querySelectorAll(".section-product-main");
+const headLine = document.querySelector(".headline");
 
 
 const cb = function(entries, observer) {
   entries.forEach(entry => {
-    if(entry.isIntersecting) {
+    if(entry.isIntersecting) { //SectionTopTitleが画面上に入ってきた時イベント
       SectionTopLists.forEach((SectionTopList, i) => {
         SectionTopList.classList.add(`boundAnimation${i}`);
       });
-    } else {
-      console.log("outview");
+      tl.fromTo(SectionProductMain, 1, {x: "-120%"}, {x: "0%", ease: Power2.easeInOut}, "+=2")
+        .to(SectionProductMain, 1, {opacity: "1", ease: Power2.easeInOut}, "-=2")
+      observer.unobserve(entry.target); //監視の停止 entry.target = SectionTopTitleのこと
     }
   });
 }
-const io = new IntersectionObserver(cb);
+const options = {
+  root: null,
+  rootMargin: "0px 0px -500px 0px", //画面の内側、下から300px監視対象の範囲
+  threshold: 1, //SectionTopTitleの下辺が500px内に入りきった時
+};
+const io = new IntersectionObserver(cb, options);
 io.observe(SectionTopTitle);
