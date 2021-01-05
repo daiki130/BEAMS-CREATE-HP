0cd98dc0c0275c842f1c408fd595ea596bb87631
daiki130 <daikibaseball130@gmail.com>
2021-01-05 23:50:12 +0900

    hero 3Danimation

--- js/header.js
+++ js/header.js
@@ -9,11 +9,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const tl = new TimelineMax();
 
   tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
-    .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
-    .fromTo(slider,1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
-    .fromTo(logo, 0.5, {opacity: "0", x: 30}, {opacity: "1", x: 0, ease: Power2.easeInOut}, "-=0.5")
+    .fromTo(hero, 1.3, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
+    .fromTo(slider,1.3, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
+    .fromTo(logo, 0.6, {opacity: "0", x: 30}, {opacity: "1", x: 0, ease: Power2.easeInOut}, "-=0.5")
+    .fromTo(headline, 0.1, {opacity: "0", x: 30}, {opacity: "1", x: 0, ease: Power2.easeInOut}, "-=0.5")
     .fromTo(hamburger, 0.5, {opacity: "0", x: 30}, {opacity: "1", x: 0, ease: Power2.easeInOut}, "-=0.5")
-    .fromTo(headline, 0.5, {opacity: "0", x: 30}, {opacity: "1", x: 0, ease: Power2.easeInOut}, "-=0.5")
 
   let ptionsObject = {
     top: window.innerHeight,  //垂直方向
@@ -23,10 +23,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   // スクロールイベント
   const tm = TweenMax.to(body, 6, {
-    delay: -3,
+    delay: -2,
     ease: Power1.easeOut,
     onComplete: function() {
       scrollBy(ptionsObject)
     },
   })
+});
+
+const hero = document.querySelector(".hero");
+const headline = document.querySelector(".headline");
+//Popout
+hero.addEventListener("mouseenter", (e) => {
+  headline.style.transform = "translate3D(0%, -70%, 200px)";
+});
+//Popback
+hero.addEventListener("mouseleave", (e) => {
+  headline.style.transform = "translate(-20%, -70%)";
+});
+
+//Animate In smartphone
+hero.addEventListener("ontouchmove", (e) => {
+  headline.style.transform = "translate3D(0%, -70%, 200px)";
+});
+//Animate Out smartphone
+hero.addEventListener("ontouchend", (e) => {
+  headline.style.transform = "translate(-20%, -70%)";
 });
