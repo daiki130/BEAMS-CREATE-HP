41738a97124d6f319c2e8e0ad8162bbf07dcc236
daiki130 <daikibaseball130@gmail.com>
2021-01-13 12:47:39 +0900

    shoesSize Relation

--- js/shoesContainer.js
+++ js/shoesContainer.js
@@ -24,10 +24,10 @@ cards.forEach(card => {
   });
   //Animate In 
   card.addEventListener("mouseenter", (e) => {
-    let cardHeaderTitle = card.querySelector(".card-header-Title");
+    // let cardHeaderTitle = card.querySelector(".card-header-Title");
     let shoesImg = card.querySelector(".shoes-img");
     //Popout
-    cardHeaderTitle.style.transform = "translate3D(5px, -5px, 100px)";
+    // cardHeaderTitle.style.transform = "translate3D(5px, -5px, 100px)";
     shoesImg.style.transform = "translate3D(0px, -20px, 100px) rotateZ(-45deg)";
   });
   card.addEventListener("mouseout", (e) => {
@@ -36,10 +36,18 @@ cards.forEach(card => {
   });
   //Animate Out
   card.addEventListener("mouseleave", (e) => {
-    let cardHeaderTitle = card.querySelector(".card-header-Title");
+    // let cardHeaderTitle = card.querySelector(".card-header-Title");
     let shoesImg = card.querySelector(".shoes-img");
     //Popback
-    cardHeaderTitle.style.transform = "translateZ(0px)";
+    // cardHeaderTitle.style.transform = "translateZ(0px)";
     shoesImg.style.transform = "translateZ(0px) rotateZ(0deg)";
+  });
+});
+
+// sneakerのsizeをクリックするとボタンの色が変化するイベント
+const sizesButtons = document.querySelectorAll(".sizesButton");
+sizesButtons.forEach(sizesButton => {
+  sizesButton.addEventListener("click", () => {
+    sizesButton.classList.toggle("active");
   });
 });
