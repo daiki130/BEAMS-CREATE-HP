6afd714c35b5415e30dc0962ba275d1284434c16
daiki130 <daikibaseball130@gmail.com>
2021-01-26 20:31:49 +0900

    AnimationSecondChange

--- js/dist/headerMenuScroll.dev.js
+++ js/dist/headerMenuScroll.dev.js
@@ -6,20 +6,20 @@
     gsap.to(el, {
       color: "salmon",
       scrollTrigger: {
-        trigger: ".section-containerFramework",
+        trigger: ".section-containerFramework", //イベントの引き金
         start: "top 70px",
         end: "bottom 70px",
-        markers: true,
+        // markers: true,
         toggleActions: "play pause resume reset"
       }
     });
     gsap.to(el, {
       color: "#FFC107",
       scrollTrigger: {
-        trigger: ".shoes-container",
+        trigger: ".shoes-container", //イベントの引き金
         start: "top 70px",
         end: "bottom 70px",
-        markers: true,
+        // markers: true,
         toggleActions: "play pause resume reset"
       }
     });
