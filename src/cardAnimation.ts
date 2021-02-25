457a067604ce7e4e3e4cb185d8d76a51558edaa6
daiki130 <daikibaseball130@gmail.com>
2021-02-25 21:03:23 +0900

    TsSetting

--- src/cardAnimation.ts
+++ src/cardAnimation.ts
@@ -1,36 +1,36 @@
-const cards = document.querySelectorAll(".card");
-const Cursor = document.querySelector(".cursor");
-const body = document.querySelector("body");
+const cards: NodeListOf<HTMLElement> = document.querySelectorAll(".card");
+const Cursor = document.querySelector(".cursor") as HTMLDivElement;
+const body = document.querySelector("body") as HTMLBodyElement;
 
-function AnimateIn(e) {
+function AnimateIn(e: Element) {
   Cursor.style.display = "none";
   body.style.cursor = "pointer";
-  let cardHeaderTitle = e.querySelector(".card-header-Title");
-  let shoesImg = e.querySelector(".shoes-img");
-  let sizesButtons = e.querySelector(".sizes");
-  let purchaseButton = e.querySelector(".purchaseButton");
+  let cardHeaderTitle = e.querySelector(".card-header-Title") as HTMLElement;
+  let shoesImg = e.querySelector(".shoes-img") as HTMLElement;
+  let sizesButtons = e.querySelector(".sizes") as HTMLDivElement;
+  let purchaseButton = e.querySelector(".purchaseButton") as HTMLButtonElement;
   //Popout
   cardHeaderTitle.style.transform = "translateZ(50px)";
   shoesImg.style.transform = "translate3D(0px, -25px, 40px) rotateZ(-45deg)";
   sizesButtons.style.transform = "translateZ(30px)";
   purchaseButton.style.transform = "translateZ(20px)";
-} 
-function AnimateOut(e) {
+}
+function AnimateOut(e: Element) {
   Cursor.style.display = "block";
   body.style.cursor = "none";
-  let cardHeaderTitle = e.querySelector(".card-header-Title");
-  let shoesImg = e.querySelector(".shoes-img");
-  let sizesButtons = e.querySelector(".sizes");
-  let purchaseButton = e.querySelector(".purchaseButton");
+  let cardHeaderTitle = e.querySelector(".card-header-Title") as HTMLElement;
+  let shoesImg = e.querySelector(".shoes-img") as HTMLElement;
+  let sizesButtons = e.querySelector(".sizes") as HTMLDivElement;
+  let purchaseButton = e.querySelector(".purchaseButton") as HTMLButtonElement;
   //Popback
   cardHeaderTitle.style.transform = "translateZ(0px)";
   shoesImg.style.transform = "translateZ(0px) rotateZ(0deg)";
   sizesButtons.style.transform = "translateZ(0px)";
   purchaseButton.style.transform = "translateZ(0px)";
-} 
+}
 
-cards.forEach(card => {
-  //Animate In 
+cards.forEach((card) => {
+  //Animate In
   card.addEventListener("mouseenter", (e) => {
     AnimateIn(card);
   });
@@ -49,8 +49,10 @@ cards.forEach(card => {
 });
 
 // sneakerのsizeをクリックするとボタンの色が変化するイベント
-const sizesButtons = document.querySelectorAll(".sizesButton");
-sizesButtons.forEach(sizesButton => {
+const sizesButtons: NodeListOf<HTMLElement> = document.querySelectorAll(
+  ".sizesButton"
+);
+sizesButtons.forEach((sizesButton) => {
   sizesButton.addEventListener("click", () => {
     sizesButton.classList.toggle("active");
   });
