aa79f3220db84c0834701a57af4bfadb95bcb410
daiki130 <daikibaseball130@gmail.com>
2021-02-27 15:52:56 +0900

    Cursor.tsを作成し、cursorの動作処理をまとめました。

---
+++ src/CursorEvent.ts
@@ -0,0 +1,41 @@
+// 要素の取得
+const cursor = document.querySelector(".cursor")! as HTMLElement;
+const footer = document.querySelector("footer")! as HTMLElement;
+const footerR = document.querySelector(".footerR")! as HTMLElement;
+const gridColumns: NodeListOf<HTMLElement> = document.querySelectorAll(
+  ".grid-column"
+);
+
+// gridColumnホバー時のcursorイベント
+gridColumns.forEach((gridColumn) => {
+  gridColumn.addEventListener("mouseover", () => {
+    cursor.classList.add("link-grow");
+  });
+  gridColumn.addEventListener("mouseleave", () => {
+    cursor.classList.remove("link-grow");
+  });
+});
+
+// フッターイベント時、実行する関数
+function footerMouseoverEvent() {
+  cursor.style.display = "none";
+  cursor.classList.add("cursorStyle");
+}
+function footerMouseoutEvent() {
+  cursor.style.display = "block";
+  cursor.classList.remove("cursorStyle");
+}
+
+// フッターホバー時のcursorイベント
+footer.addEventListener("mouseover", () => {
+  footerMouseoverEvent();
+});
+footer.addEventListener("mouseout", () => {
+  footerMouseoutEvent();
+});
+footerR.addEventListener("mouseover", () => {
+  footerMouseoverEvent();
+});
+footerR.addEventListener("mouseout", () => {
+  footerMouseoutEvent();
+});
