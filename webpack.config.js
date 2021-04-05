e7b60a91041d77b8546f17ff98d3d80a8d334a89
daiki130 <daikibaseball130@gmail.com>
2021-04-05 21:52:58 +0900

    ✅ webpack-setting

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
+              ],
+            },
+          },
+        ],
+      },
+      // Sass/CSSファイルの読み込みとコンパイル
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
