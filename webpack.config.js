c158582d0c45dbe5329bfc59ec12a29fd7c04dbe
daiki130 <daikibaseball130@gmail.com>
2021-04-07 21:43:46 +0900

    ♻️ レスポンシブの横幅を変更

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              sourceMap: false,
+              sourceMap: true,
             },
           },
           {
